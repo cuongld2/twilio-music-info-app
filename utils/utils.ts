8311d5260ce288d14e3343ffc9e52fd447e3db42
DonaldLe <ledinhcuong99@gmail.com>
2023-03-14 17:52:17 +0700

    Remove unnecessary files

--- utils/utils.ts
+++ utils/utils.ts
@@ -5,10 +5,3 @@ export const homeProfileImage: string = "https://imgur.com/VAXEwKT.png";
 export const navigationRoutes: string[] = [
 ];
 
-export const snippetsImages: { [key: string]: string } = {
-  css: "https://imgur.com/ArD8JIg.png",
-  js: "https://imgur.com/lFKi8mB.png",
-  react: "https://imgur.com/m2jv6MK.png",
-  ts: "https://imgur.com/Ux6L5Uh.png",
-  supabase: "https://imgur.com/xgNKVQa.png",
-};
